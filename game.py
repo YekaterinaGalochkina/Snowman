cff83462f0b64244d6d4d65fca79245d9d91be00
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-02-25 12:42:32 -0800

    update snowman

--- game.py
+++ game.py
@@ -12,17 +12,6 @@ SNOWMAN_GRAPHIC = [
     '-----------'
 ]
 
-
-def snowman(snowman_word):
-    """Complete the snowman function
-    replace "pass" below with your own code
-    It should print 'Congratulations, you win!'
-    If the player wins and, 
-    'Sorry, you lose! The word was {snowman_word}' if the player loses
-    """
-    pass
-
-
 def print_snowman_graphic(wrong_guesses_count):
     """This function prints out the appropriate snowman image 
     depending on the number of wrong guesses the player has made.
@@ -117,3 +106,32 @@ def is_word_guessed(snowman_word, correct_letter_guess_statuses):
         if not correct_letter_guess_statuses[letter]:
             return False
     return True
+
+
+def snowman(snowman_word):
+    """Complete the snowman function
+    replace "pass" below with your own code
+    It should print 'Congratulations, you win!'
+    If the player wins and, 
+    'Sorry, you lose! The word was {snowman_word}' if the player loses
+    """
+    correct_letter_guess_statuses = build_letter_status_dict(snowman_word)
+    wrong_guesses_list = []
+
+    while len(wrong_guesses_list) < SNOWMAN_MAX_WRONG_GUESSES:
+        user_input = get_letter_from_user(correct_letter_guess_statuses, wrong_guesses_list)
+        if user_input in correct_letter_guess_statuses:
+            print("You guessed a letter that's in the word!")
+            correct_letter_guess_statuses[user_input] = True
+        else:
+            print(f"The letter {user_input} is not in the word")
+            wrong_guesses_list.append(user_input)
+
+        print_snowman_graphic(len(wrong_guesses_list))
+        print(f"Wrong guesses: {wrong_guesses_list}")
+        if all(correct_letter_guess_statuses.values()):
+            print("Congratulations, you win!")
+            break
+    
+    if len(wrong_guesses_list) == SNOWMAN_MAX_WRONG_GUESSES:
+        print(f"Sorry, you lose! The word was {snowman_word}.")
